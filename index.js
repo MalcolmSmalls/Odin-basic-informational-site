3528772bb41ffee18f8e6731d0ef58ac2f7920ab
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-13 17:21:04 -0800

    Use express to display hello world

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const http = require('http')
+const express = require("express")
+const app = express()
+const port = 3000
 
-const hostname = "127.0.0.1"
-const port = 8000;
+app.get("/", (req, res) => {
+    res.send("Hello World! Working");
+});
 
-const server = http.createServer(function ( request, response) {
-    response.writeHead(200, { "Content-Type" : "text/plain"})
-
-    response.end('Hello World\n')
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
 
-server.listen(port, hostname, function () {
-    console.log(`Server running at http://${hostname}:${port}/`)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http')
+
+// const hostname = "127.0.0.1"
+// const port = 8000;
+
+// const server = http.createServer(function ( request, response) {
+//     response.writeHead(200, { "Content-Type" : "text/plain"})
+
+//     response.end('Hello World\n')
+// })
+
+// server.listen(port, hostname, function () {
+//     console.log(`Server running at http://${hostname}:${port}/`)
+// })
