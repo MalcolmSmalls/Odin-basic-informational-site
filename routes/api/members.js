053c7f4ff04eefb390a3679177e1a6962efe34ca
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-15 12:52:02 -0800

    Add ability to delete

--- routes/api/members.js
+++ routes/api/members.js
@@ -61,6 +61,20 @@ router.put('/:id', ( req, res ) => {
 )    
 
 
+// Delete
+
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if(found){
+       json.res({
+        msg: `Member deleted`,
+        members: members.filter(member => member.id !== parseInt(req.params.id))})
+    } else {
+        res.status(400).json  ({ msg: `No member with the id of ${req.params.id}`})
+    }
+})
+
 
 
 
